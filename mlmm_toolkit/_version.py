ce427bf9b34ccc99d9a89273ed5e637d32f80d4e
Dokkoisyo <chemistry.waseda@gmail.com>
2026-02-24 00:21:51 +0900

    refactor cli

--- mlmm_toolkit/_version.py
+++ mlmm_toolkit/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev2+ge48a85530.d20260220'
-__version_tuple__ = version_tuple = (0, 1, 'dev2', 'ge48a85530.d20260220')
+__version__ = version = '0.1.dev12+gf8a1ea80d.d20260223'
+__version_tuple__ = version_tuple = (0, 1, 'dev12', 'gf8a1ea80d.d20260223')
 
-__commit_id__ = commit_id = 'ge48a85530'
+__commit_id__ = commit_id = 'gf8a1ea80d'
